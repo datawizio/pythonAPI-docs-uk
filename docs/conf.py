3efbcac84c3b17efc65520caf159d2e12ea81c2e
NikitaZakharov <nekit0990507392>
2018-09-11 10:56:03 +0300

    changed docs language

--- docs/conf.py
+++ docs/conf.py
@@ -58,8 +58,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = u'ru'
-locale_dirs = ['locale/']
+language = u'uk'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
